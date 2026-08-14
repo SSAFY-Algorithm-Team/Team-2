a2c714bda860f2f5f51af16c85e7af14981bebb6
EunahCho <eunah702@gmail.com>
2026-08-14 21:46:10 +0900

    ㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇ

---
+++ EunahCho-kr/week-02/uniform.py
@@ -0,0 +1,24 @@
+def solution(n, lost, reserve):
+    uniforms = [1] * (n + 2) # 앞 뒤 패딩값 -> 탐색시 인덱스 에러 방지
+    for r in reserve:
+        uniforms[r] += 1
+
+    for l in lost:
+        uniforms[l] -= 1
+
+
+    for i in range(1, n+1):
+        if uniforms[i] == 0:
+            if uniforms[i-1] == 2: # 왼쪽 먼저 탐색
+                uniforms[i-1] = 1
+                uniforms[i] = 1
+            elif uniforms[i+1] == 2: # 안되면 오른쪽
+                uniforms[i+1] = 1
+                uniforms[i] = 1
+
+    answer = 0
+    for uniform in uniforms[1:-1]: # 패딩값 빼고, 값이 있는 개수 세기
+        if uniform:
+            answer += 1
+
+    return answer

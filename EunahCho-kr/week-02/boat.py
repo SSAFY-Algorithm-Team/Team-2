ceb8f9ac5ceab84dc59d0f123001197a1450f985
EunahCho <eunah702@gmail.com>
2026-08-17 14:28:58 +0900

    solve: [PGS Lv2] 구명보트

--- EunahCho-kr/week-02/boat.py
+++ EunahCho-kr/week-02/boat.py
@@ -1,62 +1,22 @@
 # programmers 구명보트
 # https://school.programmers.co.kr/learn/courses/30/lessons/42885
-# 시간 : 1h / 시도 : 4
+# 시간 : 1h 15m / 시도 : 5
 
-def solution(people, limit):
-    people.sort(reverse=True)
-    print(people) 
-    n = len(people)
-    survived = [False] * n
-    boat = 0
-    while any(survived):
-        for i in range(n):
-            remain = limit - people[i]
-            while remain 
-        # for j in range(i + 1, n):
-            if people[j] <= remain and not survived[j]:
-                survived[j] = True
-                # print(survived) #
-                remain -= people[j]
-        boat += 1
-
-    return boat
-
-
-""" 거의 반 테케 실패,,;
-def solution(people, limit):
-    n = len(people)
-    survived = [True] + [False] * (n - 1)
-    boats = [people[0]]
-
-    for i in range(1, n):
-        if not survived[i]:
-            for j in range(len(boats)):
-                if boats[j] + people[i] <= limit:
-                    boats[j] += i
-                    survived[i] = True
-                    break
-
-        if not survived[i]:
-            boats.append(people[i])
-
-    return len(boats)"""
-
-
-""" test case 16부터 실패
 def solution(people, limit):
     people.sort()
-    n = len(people)
-    survived = [False] * n
+    left = 0
+    right = len(people) - 1
     boat = 0
-    for i in range(n):
-        if not survived[i]:
-            survived[i] = True
-            remain = limit - people[i]
-            for j in range(n - 1, i, -1):
-                if people[j] <= remain and not survived[j]:
-                    survived[j] = True
-                    remain -= people[j]
+    while left <= right:
+        if people[left] + people[right] <= limit:
+            left += 1
+            right -= 1
+            boat += 1
+        elif people[right] <= limit:
+            right -= 1
+            boat += 1
+        elif people[left] <= limit:
+            left += 1
             boat += 1
 
     return boat
-"""

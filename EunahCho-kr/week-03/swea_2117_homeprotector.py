6d245efae55002e3e18c924966a60f0e1d1570cc
EunahCho <eunah702@gmail.com>
2026-08-28 00:17:21 +0900

    solving... 2

--- EunahCho-kr/week-03/swea_2117_homeprotector.py
+++ EunahCho-kr/week-03/swea_2117_homeprotector.py
@@ -1,6 +1,87 @@
 # SWEA 2117 홈 방범 서비스
 # https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5V61LqAf8DFAWu
-# 시간 40m / 시도 0
+# 시간 4h / 시도 2
+
+from collections import deque
+
+
+DIRECTIONS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+
+def calculate(k, home_cnt, m):
+    profit = (home_cnt * m) - (k * k + (k - 1) * (k - 1))
+    if profit >= 0:
+        return True
+    else:
+        return False
+
+
+def bfs(n, k, homes, starts):
+
+    start_r, start_c = starts
+
+    queue = deque([(start_r, start_c)])
+    visited = [[False] * n for _ in range(n)]
+    visited[start_r][start_c] = True
+    cnt = 0
+
+    while queue:
+        r, c = queue.popleft()
+        for dr, dc in DIRECTIONS:
+            nr, nc = r + dr, c + dc
+            if 0 <= nr < n and 0 <= nc < n and not visited[nr][nc]:
+                if abs(nr - start_r) + abs(nc - start_c) < k:
+                    queue.append((nr, nc))
+                    visited[nr][nc] = True
+                    if homes[nr][nc]:
+                        cnt += 1
+    return cnt
+
+
+def solve(n, m, homes):
+    if n % 2 == 0:
+        k = n + 3
+    else:
+        k = n
+
+    max_cnt = 0
+
+    while k > 0: 
+        for x in range(n):
+            for y in range(n):
+                home_cnt = bfs(n, k, homes, (x, y))
+                if calculate(k, home_cnt, m):
+                    max_cnt = max(max_cnt, home_cnt) # 첫 위치에서 시도
+                    # 가지치기 하고 싶다!!!!!!!!!!!!!
+        k -= 1
+
+    return max_cnt  
+
+
+def main():
+    T = int(input())
+    for t in range(1, T+1):
+        n, m = map(int, input().split())
+        homes = [list(map(int, input().split())) for _ in range(n)]
+        ans = solve(n, m, homes)
+        print(f"#{t} {ans}")
+        
+
+if __name__ == "__main__":
+    main()
+
+
+# 두번째 접근 : 첫번째 집에서 가장 먼 집까지 거리 - k : 이익 따져보기 => k하나씩 줄이기 ==> 두번째 집에서 ~ 
+
+"""
+집 위치 다 따기
+- 한 집에서 시작 -> k = 1거리에 있는 집 수 셈 (bfs) -> k = 2거리에,, -> k가 다 덮어버리면 
+
+"""
+
+"""
+첫시도 였던것
+# 첫 접근 : 왼쪽 맨 위에 맵 만드는 함수 -> direction 따라 움직일 수 있는 함수 만듦 -> bfs로 지정된 영역 내 집 개수 세는 함수 만듦 -> 가장 큰 맵에서 시작해서 크기를 하나씩 줄이기 + 위치 조정으로 손해가 아닌 최대 집 수 구함
 
 from collections import deque
 from pprint import pprint
@@ -114,3 +195,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+"""

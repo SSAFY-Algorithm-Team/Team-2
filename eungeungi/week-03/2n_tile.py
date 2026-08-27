941614f3fd2ef5912c6f2ee3be971b69555e89ff
eungseon <two01272@naver.com>
2026-08-27 11:35:19 +0900

    Revert "3주차 클리어"
    This reverts commit bb6d8f9cf23823cf97a945045a2ba65ab13f66da.

--- eungeungi/week-03/2n_tile.py
+++
@@ -1,9 +0,0 @@
-def solution(n):
-    answer = [0] * 60001
-    if n<=2:
-        return n
-    answer[1] = 1
-    answer[2] = 2
-    for i in range(3,n+1):
-        answer[i] = (answer[i-1] + answer[i-2]) % 1000000007
-    return answer[i] 
--- eungeungi/week-03/eat_land.py
+++
@@ -1,9 +0,0 @@
-def solution(land):
-    n = len(land)
-    dp = [[0]*4 for _ in range(n)]
-    dp[0] = land[0]
-    for i in range(1,n):
-        for j in range(4):
-            #i행 j열에 i-1행 j열과 겹치지 않는 선에서 max값 더하기
-            dp[i][j] = land[i][j] + max(dp[i-1][k] for k in range(4) if k!=j) 
-    return max(dp[-1])
